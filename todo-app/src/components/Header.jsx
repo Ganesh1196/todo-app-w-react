697db3883a0b08faa0395d3789096b1a5842e49a
Ganesh Abhange <ganesh61980@gmail.com>
2025-04-25 22:20:23 +0530

    Header Open count changed to Open Tab count

--- todo-app/src/components/Header.jsx
+++ todo-app/src/components/Header.jsx
@@ -1,7 +1,7 @@
 export function Header(props) {
     const { todos } = props
     const todosLength = todos.length
-
+    const openTodosLength = todos.filter(val => !val.complete).length
     const isTaskPlural = todosLength != 1
 
     const taskOrTasks = isTaskPlural ? "tasks" : "task"
@@ -9,7 +9,7 @@ export function Header(props) {
 
     return (
         <header>
-            <h1 className="text-gradient">You have {todosLength} open {taskOrTasks}.</h1>
+            <h1 className="text-gradient">You have {openTodosLength} open {taskOrTasks}.</h1>
 
         </header>
     )
